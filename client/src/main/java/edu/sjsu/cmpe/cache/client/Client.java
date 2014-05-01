ace27a9253474b2b2a4a5e99d3b684cee3e6c57c
Shriyansh Jain <shriyanshajain@gmail.com>
2014-04-30 17:45:58 -0700

    first

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,37 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting Cache Client...");
+		int bucket;
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+		List<CacheServiceInterface> list = new ArrayList<CacheServiceInterface>();
+		list.add(new DistributedCacheService("http://localhost:3000"));
+		list.add(new DistributedCacheService("http://localhost:3001"));
+		list.add(new DistributedCacheService("http://localhost:3002"));
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+		char value = 'a';
+		for(int hashkey=1;hashkey<=10;hashkey++)
+		{
+			bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(hashkey)), list.size());
+			list.get(bucket).put(hashkey, Character.toString(value));
 
-        System.out.println("Existing Cache Client...");
-    }
+			System.out.println("(" + hashkey +" ==> "+ value + ")");
+			value++;
+		}
+		for(int hashkey=1;hashkey<=10;hashkey++)
+		{
+			bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(hashkey)), list.size());
+			System.out.println("(" + hashkey + "==>" + list.get(bucket).get(hashkey) +")");
+		}
+
+		System.out.println("Exiting Cache Client...");
+	}
 
 }
